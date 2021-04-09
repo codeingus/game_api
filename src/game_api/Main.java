646e843fda5bf19ce6c9785e43e04c89c8710589
boyboy0710 <boyboy0710@naver.com>
2021-04-09 22:06:59 +0900

    이상한 버그수정

--- src/game_api/Main.java
+++ src/game_api/Main.java
@@ -5,7 +5,6 @@ import game_api.player.player_data_load;
 public class Main {
 
     public static void main(String[] args) {
-        player_data_load.player_data_load();
 	    basic.game();
     }
 
