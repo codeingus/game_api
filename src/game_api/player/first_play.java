b953c538a052c6394dcf2a504c0651932f675912
boyboy0710 <boyboy0710@naver.com>
2021-06-07 16:25:08 +0900

    Update first_play.java

--- src/game_api/player/first_play.java
+++ src/game_api/player/first_play.java
@@ -20,7 +20,7 @@ public class first_play {
          player_name = JOptionPane.showInputDialog("사용할 닉네임를 입력하세요");
 
          pro.setProperty("player_name", player_name);
-        //파일을 
+        //파일을 저장한다 
         try {
             pro.save( new FileOutputStream("src/game_api/resources/player_data.properties"),"");
         } catch (FileNotFoundException e) {
